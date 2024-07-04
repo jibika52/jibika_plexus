210fa134b94dc4e108123ce87dd90dcefd37a8e6
jibika52 <jibikaapp@gmail.com>
2024-07-04 17:14:23 +0600

    toggol button done

--- lib/View/BootomNatchBar/BootomNavigationBarItemsScreen/BootomNavigationBarItemsMainHomeScreen/bootom_navigation_barItems_main_home_screen.dart
+++ lib/View/BootomNatchBar/BootomNavigationBarItemsScreen/BootomNavigationBarItemsMainHomeScreen/bootom_navigation_barItems_main_home_screen.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -23,9 +24,11 @@ class _BootomNavBarItemsMainFloationHomeScreenState extends State<BootomNavBarIt
   double lineWidth=7.0;
   double C_height=5;
   bool _is_click_date=false;
-  bool _is_tear_date=false;
   int selectedmonth=0;
-
+  int value = 0;
+  int? nullableValue;
+  bool positive = false;
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
     double h=MediaQuery.of(context).size.height;
@@ -52,57 +55,40 @@ class _BootomNavBarItemsMainFloationHomeScreenState extends State<BootomNavBarIt
               child: Stack(
                 children: [
                   Container(
-                    height: 39,
+                    height: 38,
                     width: 140,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(7),
-                        // color: Colors.red,
-                        border: Border.all(
-                            color: Main_Theme_textColor.withOpacity(0.15),
-                            width: 1.5
-                        )
-                    ),
-                    padding: EdgeInsets.only(left: 7,right: 7),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CustomImageSction(height: 24, width: 24, radius: 1, image: "Assets/Icons/share2.png"),
-                        Container(
-                          height: 20,
-                          width: 1,
-                          color: Main_Theme_textColor.withOpacity(0.3),
-                        ),
-                        CustomImageSction(height: 24, width: 24, radius: 1, image: "Assets/Icons/sms.png"),
-                        Container(
-                          height: 20,
-                          width: 1,
-                          color: Main_Theme_textColor.withOpacity(0.3),
-                        ),
-                        CustomImageSction(height: 24, width: 24, radius: 1, image: "Assets/Icons/pdf.png"),
-
-                        Container(
-                          height: 20,
-                          width: 1,
-                          color: Main_Theme_textColor.withOpacity(0.3),
-                        ),
-                        Stack(
-                          children: [
-                            CustomImageSction(height: 24, width: 24, radius: 1, image: "Assets/DashBoardIcons/notification_attendence.png"),
-                            Positioned(
-                              right: 0,
-                              top: 0,
-                              child: CircleAvatar(
-                                radius: 7,
-                                backgroundColor:absent_color,
-                                child: ColorCustomText(fontSize: 7, fontWeight: FontWeight.w400, text: "22", letterSpacing: 0.1, textColor: Main_Theme_WhiteCollor),
-                              ),
-                            )
-                          ],
+                    padding: EdgeInsets.only(top: 2),
+                    child:  Container(
+                      height: 38,
+                      child: AnimatedToggleSwitch<bool>.size(
+                        current:_isActive ,
+                        values: [false,true],
+                        iconOpacity: 0.2,
+                        indicatorSize: Size.fromWidth(100),
+                        customIconBuilder: (context,local,global)=>
+                            Text(local.value? "Inactive":"Active",style: TextStyle(
+                              fontSize: 11,
+                              fontWeight: FontWeight.w400,
+                              color:Main_Theme_textColor,
+                            ),),
+                        borderWidth: 1.0,
+                        iconAnimationCurve: Curves.linear,
+                        selectedIconOpacity: 1.0,
+                        onChanged: (value) => setState(() {
+                          _isActive=!_isActive;
+                        }),
+                        styleBuilder: (b) => ToggleStyle(
+                          borderColor: Main_Theme_textColor.withOpacity(0.5),
+borderRadius: BorderRadius.circular(10),
+                          backgroundColor: b ? Colors.white : Colors.white,
+                          indicatorColor: b ? Colors.red : Colors.green,
+                          indicatorBorderRadius: BorderRadius.circular(b ? 4.0 : 4.0),
                         ),
 
-                      ],
-                    ),
+                      ),
+                    )
                   ),
+
                   Positioned(
                     top: 0.9,
                       right: 0,
@@ -204,10 +190,142 @@ class _BootomNavBarItemsMainFloationHomeScreenState extends State<BootomNavBarIt
                 ],
               ),
             ),
-            Expanded(child: Container(color: Colors.deepOrangeAccent,))
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(top: 7),
+                width: 400,
+                color: Main_Theme_WhiteCollor,
+                child: Container(
+                    child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all( Radius.circular(7)),
+
+                              color:  CustomButtonColor.withOpacity(0.05),
+                              border: Border(bottom: BorderSide( color: CustomButtonColor))
+                          ),
+                          margin: EdgeInsets.only(bottom: 7),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(left: 10,right: 10,top: 5,bottom: 5),
+                                height: 80,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(7),
+
+                                  color:  leave_color.withOpacity(0.05),
+                                  //  border: Border(bottom: BorderSide(color:_selectedindex==index?CustomButtonColor.withOpacity(0.05): CustomButtonColor))
+                                ),
+                                width: double.infinity,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: 65,
+                                      width: 52,
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.all(Radius.circular(7))
+                                      ),
+                                      margin: EdgeInsets.only(right: 10),
+                                      child: Stack(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(1.0),
+                                            child: ClipRRect(
+                                                borderRadius: BorderRadius.circular(100),
+                                                child: CustomImageSction(height: 64, width: 50, radius: 1, image: "Assets/DrawerImage/trackingimage.png")
+                                            ),
+                                          ),
+                                          Positioned(
+                                              left: 4.0,
+                                              top: 1.6,
+                                              child: ClipRRect(
+                                                borderRadius: BorderRadius.circular(100),
+                                                child: Container(
+                                                  height: 45,
+                                                  width: 45,
+                                                  decoration: BoxDecoration(
+                                                      color: Colors.red,
+                                                      image: DecorationImage(image: AssetImage("Assets/DrawerImage/testperson.png"),fit: BoxFit.fill)
+                                                  ),
+                                                ),
+                                              ))
+
+                                        ],
+                                      ),
+                                    ),
+
+
+                                    Expanded(
+                                      flex: 4,
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Container(
+                                            height: 22,
+                                            width: 65,
+                                            decoration: BoxDecoration(
+                                                borderRadius: BorderRadius.circular(10),
+                                                color:Main_Theme_textColor_tir_Condition.withOpacity(0.7)
+                                            ),
+                                            alignment: Alignment.center,
+                                            child: ColorCustomText(fontSize: 12, fontWeight: FontWeight.w400, text: "544532", letterSpacing: 0.3,textColor: Main_Theme_WhiteCollor,),
+                                          ),
+                                          SizedBox(height: 3,),
+                                          Text("Hafijur Rahman Mizan",
+                                            overflow: TextOverflow.ellipsis
+                                            ,style: GoogleFonts.poppins(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.w500,
+                                              letterSpacing: 0.3,
+                                            ),),
+                                          SizedBox(height: 1,),
+                                          CustomText(fontSize: 11, fontWeight: FontWeight.w300, text: "HR Manager", letterSpacing: 0.3,  ),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(width: 10,),
+
+
+                                    Container(
+                                      height: 38,
+                                      width: 110,
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(10),
+                                          color: Main_Theme_textColor_tir_Condition.withOpacity(0.7)
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          ColorCustomText(fontSize: 14, fontWeight: FontWeight.w400, text: "Track Now", letterSpacing: 0.3, textColor: Main_Theme_WhiteCollor),
+                                          SizedBox(width: 5,),
+                                          Icon(Icons.arrow_forward_ios_rounded,size: 17,color: Main_Theme_WhiteCollor,),
+                                        ],
+                                      ),
+                                    )
+
+
+                                  ],
+                                ),
+                              ),
+
+                            ],
+                          ),
+                        );
+                      },)
+                ),
+              ),
+            )
+
           ],
         ),
       )
     );
   }
+  bool  isActive=true;
+  bool _isActive=false;
+
 }
